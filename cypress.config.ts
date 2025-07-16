737aed043fabaf08d2aaa874276e881bd21dee2f
rajiv <rajiv.sapkota96@gmail.com>
2025-07-16 15:48:24 +0545

    added mochaawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "cypress";
 import * as dotenv from "dotenv";
+const {
+  beforeRunHook,
+  afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
+
 
 dotenv.config();
 
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "API Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     env: {
       token: process.env.token,
@@ -12,7 +25,15 @@ export default defineConfig({
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.ts",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
+
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
     },
   },
 });
