a33635694ae580e55f1d9c352226e8cb0d43071d
rajiv <rajiv.sapkota96@gmail.com>
2025-07-16 14:50:06 +0545

    integrated faker js

---
+++ cypress/fakerData/data.ts
@@ -0,0 +1,14 @@
+// generate-fake-user.ts
+import { faker } from "@faker-js/faker";
+
+export function generateFakeuser() {
+  return {
+    username: faker.internet.username(),
+    password: faker.internet.password({ length: 12 }),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    zipCode: faker.location.zipCode(),
+    email: faker.internet.email(),
+    gender: faker.helpers.arrayElement(["male", "female"]),
+  };
+}
